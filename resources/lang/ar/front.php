d7372bdfbcaf1cffb407c6079359d0f6a9440982
mohamdfawaz <mohamdfawaz93@gmail.com>
2020-12-25 14:13:53 +0200

    Added arabic resources locale

---
+++ resources/lang/ar/front.php
@@ -0,0 +1,247 @@
+<?php
+
+return [
+    'head' =>
+        [
+            'title' =>
+                [
+                    'name' => 'OVAK'
+                ]
+        ],
+    'header' =>
+        [
+            'home' => 'Home',
+            'developers' => 'Developers',
+            'projects' => 'Projects',
+            'consultancy' => 'Consultancy',
+            'about_contact' => 'About/Contact',
+            'about' => 'About',
+            'filter_result' => 'Filter Result',
+            'logout' => 'Logout',
+            'login' => 'Login',
+            'inner_header' => [
+                'consultancy' => [
+                    'consultancy' => 'Consultancy',
+                    'service' => 'Service'
+                ]
+            ]
+        ],
+    'login' =>
+        [
+            'sign_up' => 'Sign up',
+            'sign_in' => 'Sign in to',
+            'hello' => 'Hello',
+            'join' => 'Join',
+            'text' => 'now and let our family show your family the way home',
+            'action' => [
+                'login' => 'Login'
+            ],
+            'email' => 'Please Enter Your E-mail Address',
+            'password' => 'Please Enter Your Password'
+        ],
+    'forget_password' =>
+        [
+            'reset_password' => 'Reset Password',
+            'text' => 'We are going to send you a verification E-mail to recover your password',
+            'forgot_password' => 'Forgot password ?'
+
+        ],
+    'verification' =>
+        [
+            'header' => 'Enter the verification code you received',
+            'input' => 'Enter verification Code'
+        ],
+    'register' =>
+        [
+            'registration' => 'Registration',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'email_address' => 'E-mail Address',
+            'phone_number' => 'Phone Number',
+            'password' => 'Password',
+            'action' => [
+                'sign_up' => 'Sign up',
+            ],
+            'welcome_back' => 'Welcome Back',
+            'text' => 'Login now and find out more choices and features from',
+            'website' => 'website'
+        ],
+
+    'logout' =>
+        [
+            'are_you_sure' => "Are you sure you want to",
+            'logout' => 'Logout'
+        ],
+    'actions' =>
+        [
+            'show_more' => 'Show More'
+        ],
+    'title' =>
+        [
+            'developers_list' => 'Developers List',
+            'projects' => 'Projects',
+            'developers' => 'Developers',
+            'view_all_developers' => 'View All Developers',
+            'view_all_projects' => 'View all properties'
+        ],
+    'filter' =>
+        [
+            'development_company' => 'Development Company',
+            'district_location' => 'District Location',
+            'unit_type' => 'Unit Type',
+            'delivery_date' => 'Delivery date',
+            'search' => 'Search',
+            'finishing_type' => 'Finishing Type',
+            'title' => 'SEARCH FOR YOUR PROPERTY'
+        ],
+    'form' =>
+        [
+            'sort_by' => 'Sort By',
+            'show_all' => 'Show All'
+        ],
+    'home' =>
+        [
+            'relax_and_let' => 'Relax & Let',
+            'do_the_rest' => 'Do the rest.',
+            'consultancy' => 'Consultancy',
+            'service' => 'Service',
+            'learn_more' => 'Learn More',
+            'vision' => [
+                'we_find_you' => 'we find you',
+                'the_perfect_choice' => 'the perfect choice',
+                'description_gold' => 'Own variable advanced knowledge',
+                'description_gray' => 'to find your perfect unit',
+                'description' => 'You search, we counsult, you get the keys to your dream ',
+                'mission_and_vision' => 'Mission & Vision'
+            ],
+            'project' => [
+                'projects' => 'projects',
+            ],
+            'subscription' => [
+                'subscribe' => 'SUBSCRIBE',
+                'market_news' => 'market news',
+                'title' => 'Get updated with the newest marketplaces, properties and the latest offers, just with a click'
+            ],
+            'developers' => [
+                'projects' => 'Projects',
+                'developers' => 'Developers',
+            ]
+        ],
+    'projects' =>
+        [
+            'district' => 'District',
+            'property_type' => 'Type of Property',
+            'one_of' => 'One Of ',
+            'projects' => 'Projects',
+            'interested' => 'Interested',
+            'ask_about_this_project_details' => 'ask about this project details',
+            'ask_now' => 'ask now',
+            'overview' => 'Overview',
+            'amenities' => 'Amenities',
+            'utilities' => 'Utilities',
+            'previous' => 'Previous',
+            'next' => 'Next',
+            'list_developers_paragraph' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+             justo duo dolores et ea rebum.',
+        ],
+    'units' => [
+        'overview' => 'Overview'
+    ],
+    'result' =>
+        [
+            'residential' => 'Residential',
+            'compound' => 'Compound'
+        ],
+    'consultancy' =>
+        [
+            'consult' => 'Consult',
+            'form' => [
+                'title' => 'let us find you the perfect choice',
+                'choices' => [
+                    'residential' => 'Residential',
+                    'commercial' => 'Commercial'
+                ],
+                'action' => [
+                    'submit' => 'CONSULT NOW'
+                ]
+            ],
+            'installment' => [
+                'installment' => 'Installments',
+                'calculator'=>'calculator',
+                'form' => [
+                    'property_price' => 'Property Price',
+                    'currency' => 'EGP',
+                    'total_amount' => 'Total Amount',
+                    'total_amount_placeholder' => 'Amount EGP',
+                    'down_payment' => 'Down Payment',
+                    'amount' => 'Amount',
+                    'amount_placeholder' => 'Amount EGP',
+                    'percent' => 'Percentage',
+                    'installment_period' => 'Installment Period',
+                    'years' => 'Years',
+                    'years_placeholder' => 'Enter Years',
+                    'actions' => [
+                        'calculate' => 'Calculate',
+                        'clear' => 'Clear'
+                    ]
+                ],
+                'table' => [
+                    'remaining' => 'Remaining',
+                    'installment_type' => 'Installment Type',
+                    'amount' => 'Amount',
+                    'number' => 'Number',
+                    'monthly_installment' => 'Monthly Installment',
+                    'quarterly_installment' => 'Quarterly Installment',
+                    'semi_annual_installment' => 'Semi Annual Installment',
+                    'le' => 'LE'
+                ]
+            ]
+        ],
+    'message' => [
+        'unauthorized_user' => 'Please check your email and password'
+    ],
+    'about' => [
+        'about_section' => ' is a promising startup real-estate consultation agency founded by youthful visionary
+      Egyptian hands, yet have eminently great futuristic business thoughts and visions embracing newfangled
+       innovative ways to get our own footprints successfully engraved within the solid grounds of the rapid
+       growing real–estate world. We strive to provide the most valuable thorough advanced real–estate knowledge
+       to facilitate your journey of searching and collecting the market info to comply with your needs, all
+       through one portfolio. OVAK widely and simply opens for you the door of a huge data-base for the most successful
+       names and biggest well-established real-estate powerhouses and developers, in order to reach your perfect tailored consultation.',
+        'slogan' => 'Own Valuable Advanced Knowledge',
+        'our_vision_1' => 'OVAK is not a new Real Estate brokerage company, we’re not seeking ripping off customers with extra fees in-return
+        of our services.',
+        'our_vision_2' => 'is not just a slogan, it’s an idea, mindset and mentality each and every individual in
+        OVAK has it. Our vision is to make information available for the end-user and minimizing the hassle of un-needed tasks. Narrowing the searching
+         is not an easy task, but by compiling experience and analyzing tons of data, knowing the customer needs is easy as a pie. knowledge is a double
+          edged weapon but with OVAK we make it Advanced in order to wield it the way we need.',
+
+        'our_mission' => 'We mainly direct our focus on providing our users with the best market experience by only “few clicks” saving their
+        search time, avoiding the unneeded hassle wasted in web searching and live showings. We offer the quintessence of years of experience
+        providing our users with the most detailed information giving easy access for the end-user within the complicated Egyptian real estate
+        market, by showing full detailed updated availability of the wide variable range of market options and units that meet all users’ variable
+         needs and conveniences.'
+
+    ],
+
+    'modals' => [
+        'logout' => [
+
+        ],
+        'login' => [
+
+        ],
+        'register' => [
+        ],
+        'ask' => [
+            'header' => 'One of our agents is going to contact you as soon as possible',
+            'sub_header' => 'Within 24 hours'
+        ],
+        'success' => [
+            'header' => 'Compeleted successfully ',
+
+        ]
+
+    ]
+];
